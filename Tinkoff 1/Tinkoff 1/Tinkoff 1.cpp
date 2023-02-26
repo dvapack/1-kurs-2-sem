95f4c403b2f6d30c9026d808d1ace6204dce62bc
dvapack <67041431+dvapack@users.noreply.github.com>
2023-02-26 20:37:11 +0400

    Tinkoff Entrance Exam

---
+++ Tinkoff 1/Tinkoff 1/Tinkoff 1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int length;
+	cin >> length;
+	if (length % 3)
+		cout << 0;
+	else
+		cout << (int)pow(2, length/3);
+}
