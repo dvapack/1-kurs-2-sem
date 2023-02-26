dbec6d2b37e55e8960cb7e2bebdf019c3914591e
dvapack <67041431+dvapack@users.noreply.github.com>
2023-02-26 20:32:28 +0400

    Tinkoff Contest 2

---
+++ Tinkoff 2-2/Tinkoff 2-2/Tinkoff 2-2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void input(int& n)
+{
+	cin >> n;
+}
+
+void not_fast(int& n, vector<int>& vec)
+{
+	if (n < 5)
+		for (int i = 0; i < n; ++i)
+			vec[i] = i + 1;
+	if (n >= 5)
+	{
+		for (int i = 0; i < n; ++i)
+			vec[i] = i + 1;
+		for (int i = 5; i <= n; ++i)
+		{
+			if (i % 2 == 0)
+				swap(vec[i / 2 - 1], vec[i - 1]);
+			if (i % 2 != 0)
+				swap(vec[i / 2], vec[i - 1]);
+		}
+	}
+}
+
+
+int main()
+{
+	int n;
+	input(n);
+	vector <int> vec(n);
+	not_fast(n, vec);
+	for (auto& s : vec)
+		cout << s << ' ';
+}
