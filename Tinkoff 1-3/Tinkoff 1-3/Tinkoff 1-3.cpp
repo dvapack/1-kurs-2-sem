7efb2432c21c4a4fe5a1298110519c14524e64f3
dvapack <67041431+dvapack@users.noreply.github.com>
2023-02-26 20:13:53 +0400

    Tinkoff Contest 1

---
+++ Tinkoff 1-3/Tinkoff 1-3/Tinkoff 1-3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	// бинарный поиск
+	int s = 1;
+	int e = n + 1;
+	int m = 0; // просто элемент опорный
+	string answer;
+	while (s + 1 < e)
+	{
+		m = (s + e) / 2;
+		cout << m << endl;
+		fflush(stdout);
+		cin >> answer;
+		if (answer == "<")
+			e = m;
+		if (answer == ">=")
+			s = m;
+	}
+	if (n == 1)
+		cout << "! 1" << endl;
+	if (answer == ">=")
+	{
+		cout << "! " << m << endl;
+	}
+	if (answer == "<")
+	{
+		cout << "! " << m - 1  << endl;
+	}
+}
