c70bf0e80af405ac7a1a1f5311a63f6cd7bfe421
dvapack <67041431+dvapack@users.noreply.github.com>
2023-02-26 20:35:07 +0400

    Языки программирования и структуры данных ЛР0

---
+++ YAPISD 0/YAPISD 0/YAPISD 0.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main()
+{
+    int a;
+    int b;
+    fstream fin("Input.txt");
+    fin >> a >> b;
+    fin.close();
+    fstream fout("Output.txt", ios::out);
+    fout << a + b;
+    fout.close();
+}
