3301317e45f322e85c0dc605271000252424c9e0
dvapack <67041431+dvapack@users.noreply.github.com>
2023-02-26 20:11:29 +0400

    Tinkoff Contest 1

---
+++ Tinkoff 1-1/Tinkoff 1-1/Tinkoff 1-1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	long long n_size, k_size;
+	cin >> n_size;
+	cin >> k_size;
+	vector <long long> n(n_size);
+	vector <long long> k(k_size);
+	for (long long i = 0; i < n_size; ++i)
+	{
+		cin >> n[i];
+	}
+	for (long long i = 0; i < k_size; ++i)
+	{
+		cin >> k[i];
+	}
+	// бинарный поиск
+	long long s = 0;
+	long long e = n_size;
+	long long m; // просто элемент опорный
+	bool flag = false;
+	for (long long i = 0; i < k_size; ++i)
+	{
+		flag = false;
+		s = 0;
+		e = n_size;
+		while (s + 1 <= e)
+		{
+			m = (s + e) / 2;
+			if (k[i] == n[m])
+			{
+				cout << "YES" << endl;
+				flag = true;
+				break;
+			}
+			if ((k[i] < n[m + 1]) && (k[i] >= n[s]))
+				e = m;
+			if ((k[i] > n[m]) && (k[i] <= n[n_size - 1]))
+				s = m;
+			if (!s && e != m)
+				break;
+		}
+		if (!flag)
+			cout << "NO" << endl;
+	}
+}
+
