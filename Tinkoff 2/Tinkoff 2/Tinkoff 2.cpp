945e56d0e8cc6c82ee855ef08251a1b75767f182
dvapack <67041431+dvapack@users.noreply.github.com>
2023-02-26 20:38:35 +0400

    Tinkoff Entrance Exam

---
+++ Tinkoff 2/Tinkoff 2/Tinkoff 2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int cars;
+    cin >> cars;
+    vector <int> speeds(cars);
+    for (int i = 0; i < cars; ++i)
+    {
+        cin >> speeds[i];
+        if (i && speeds[i] > speeds[i - 1])
+            speeds[i] = speeds[i-1];
+    }
+    for (int i = 0; i < cars; ++i)
+    {
+        cout << speeds[i] << ' ';
+    }
+}
